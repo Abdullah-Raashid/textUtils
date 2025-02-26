d8425467358bf9dd6b7ac5e42ea20ce73a9d7f63
tejas-mani-co <tejas@kkmani.com>
2025-02-27 01:25:11 +0900

    update

--- textutils/settings.py
+++ textutils/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure--o6_7r9pph_#bkd7vf-k-y@+x5e#3(g2wv_x0=+o(4*nawj_b="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['.vercel.app', 'localhost']
 
 # Application definition
 
